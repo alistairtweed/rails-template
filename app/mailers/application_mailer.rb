27dd55760941dd2d8554a324ebc7d676dd94b852
Alistair Tweed <email@alistairtweed.com>
2020-07-29 20:43:45 +0800

    Cleans up files

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV.fetch('SMTP_ADDRESS') { 'from@example.com' }
   layout 'mailer'
 end
